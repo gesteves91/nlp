f148f9bd146b132b60c0be91defb12267f8dc4f9
Geanderson Esteves <esteves.gean@gmail.com>
2019-10-10 12:50:04 -0300

    hide the solutions from my students

--- solutions/exercise-01/er02.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def is_allowed_specific_char(string):
-    charRe = re.compile(r'[^a-zA-Z0-9.]')
-    string = charRe.search(string)
-    return not bool(string)
-
-print(is_allowed_specific_char("ABCDEFabcdef123450")) 
-print(is_allowed_specific_char("*&%@#!}{"))
--- solutions/exercise-01/er08.py
+++
@@ -1,5 +0,0 @@
-import re
-
-text = "PythonTutorialAndExercises"
-
-print(re.findall('[A-Z][^A-Z]*', text))
